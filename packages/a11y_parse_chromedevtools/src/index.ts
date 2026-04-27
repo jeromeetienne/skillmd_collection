5f989a567ec4b5485f47e4b31efd51063342e1e8
Jerome Etienne <jerome.etienne@gmail.com>
2026-04-27 11:05:04 +0200

    refactor:remove obsolete files

--- packages/a11y_parse_chromedevtools/src/index.ts
+++
@@ -1,3 +0,0 @@
-export type { AxNode, AxTreeTestFn } from './libs/a11y_tree';
-export { A11yTree } from './libs/a11y_tree';
-export { A11yQuery } from './libs/a11y_selector';
--- packages/fastbrowser_cli copy/src/fastbrowser_mcp/fastbrowser_types.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Defines the types used in the FastBrowser MCP implementation.
- */
-export type FastBrowserMcpTarget = 'chrome_devtools' | 'playwright';
