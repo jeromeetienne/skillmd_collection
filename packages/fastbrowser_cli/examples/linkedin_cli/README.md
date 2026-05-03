fc337d47ed1f0a468600befad73d5f98bd37fa38
Jerome Etienne <jerome.etienne@gmail.com>
2026-05-03 04:32:26 +0200

    feat: update README.md with LinkedIn DM CLI commands; add linkedin_full_cycle.sh script for example usage

--- packages/fastbrowser_cli/examples/linkedin_cli/README.md
+++ packages/fastbrowser_cli/examples/linkedin_cli/README.md
@@ -1,1 +1,7 @@
 - https://www.lemlist.com/fr/pricing - parisjs / france
+
+# Linkedin DM CLI
+- linkedin_dm.ts
+  - list_convos
+  - send <target_user> <message>
+  - messages <target_user> "
---
+++ packages/fastbrowser_cli/examples/linkedin_cli/linkedin_full_cycle.sh
@@ -0,0 +1,5 @@
+
+
+npx tsx ./examples/linkedin_cli/linkedin_dm.ts list_convos
+npx tsx ./examples/linkedin_cli/linkedin_dm.ts messages "Eric Defiez"
+npx tsx ./examples/linkedin_cli/linkedin_dm.ts send "Eric Defiez" "Hello from linkedin_dm.ts"
